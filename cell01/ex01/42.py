6d5af9b09af1683d555d243ce4afb3415b6eb6f2
Gamets13 <wem4018@gmail.com>
2025-06-17 05:23:47 +0000

    add 42.py

--- cell01/ex01/42.py
+++ cell01/ex01/42.py
@@ -1,4 +1,4 @@
-last_name = "Doe"
-first_name = "John"
-whole_name = last_name + first_name
+last_name = "Thada"
+first_name = "Saeli"
+whole_name = last_name + " " + first_name
 print(whole_name + "\n")
