8966d03905b7a7d634fe9cc88a89290d65ac2152
Gamets13 <wem4018@gmail.com>
2025-06-18 08:03:45 +0000

    add mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter tha first nunber :"))
+num2 = int(input("Enter tha second nunder :"))
+
+print (f"{num1} x {num2} = {num1 * num2 }")
+if num1 * num2 > 0:
+  print("Tha result is positive.")
+elif num1 * num2 < 0:
+  print ("Tha result is negative.")  
+else :
+  print ("Tha result is posisive and negative")  
