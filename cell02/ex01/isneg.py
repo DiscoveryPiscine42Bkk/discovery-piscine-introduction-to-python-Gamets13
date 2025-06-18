dfd182a4c1a5ec2b473e48709fb9895b2883da2e
Gamets13 <wem4018@gmail.com>
2025-06-18 07:38:30 +0000

    add isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+value = int(input())
+
+if value > 0 :
+  print ( "Thia number is positive.")
+elif value < 0 :
+  print ("Thai number is negetive.")
+else:
+  print (" Thia number is both positive and negetive ")   
